09cca9293d0cee3346bce8e98c053d8156451882
stnDinus <111202214433@mhs.dinus.ac.id>
2024-09-30 17:13:39 +0700

    feat: biodata

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <small>
+        Steven Nugraha - A11.2022.14433
+      </small>
     </>
   )
 }
