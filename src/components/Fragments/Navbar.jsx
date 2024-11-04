c68ec0c8978a6b79ea999953f5fad9018bd97a8f
stnDinus <111202214433@mhs.dinus.ac.id>
2024-11-04 16:13:50 +0700

    feat: sticky nav

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -49,50 +49,52 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between">
-      <div>
-        <div className="flex justify-center mb-10">
-          <Logo variant="text-white text-sm sm:text-2xl" />
+    <div className="bg-defaultBlack">
+      <nav className="sticky top-0 text-special-bg2 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between">
+        <div>
+          <div className="flex justify-center mb-10">
+            <Logo variant="text-white text-sm sm:text-2xl" />
+          </div>
+          {menus.map((menu) => (
+            <NavLink
+              to={menu.link}
+              key={menu.id}
+              className={({ isActive }) =>
+                isActive
+                  ? "flex bg-primary text-white font-bold px-4 py-3 rounded-md"
+                  : "flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md"
+              }
+            >
+              <div className="mx-auto sm:mx-0">{menu.icon}</div>
+              <div className="ms-3 hidden sm:block">{menu.label}</div>
+            </NavLink>
+          ))}
         </div>
-        {menus.map((menu) => (
-          <NavLink
-            to={menu.link}
-            key={menu.id}
-            className={({ isActive }) =>
-              isActive
-                ? "flex bg-primary text-white font-bold px-4 py-3 rounded-md"
-                : "flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md"
-            }
-          >
-            <div className="mx-auto sm:mx-0">{menu.icon}</div>
-            <div className="ms-3 hidden sm:block">{menu.label}</div>
-          </NavLink>
-        ))}
-      </div>
-      <div className="sticky bottom-12">
-        <NavLink to="/logout" />
-        <div className="flex bg-special-bg3 px-4 py-3 rounded-md hover:text-white">
-          <div className="mx-auto sm:mx-0">
-            <Icon.Logout />
+        <div className="sticky bottom-12">
+          <NavLink to="/logout" />
+          <div className="flex bg-special-bg3 px-4 py-3 rounded-md hover:text-white">
+            <div className="mx-auto sm:mx-0">
+              <Icon.Logout />
+            </div>
+            <div className="ms-3 hidden sm:block">Logout</div>
           </div>
-          <div className="ms-3 hidden sm:block">Logout</div>
-        </div>
-        <NavLink />
-        <div className="border-b my-10 border-b-special-bg"></div>
-        <div className="flex justify-between">
-          <div className="mx-auto sm:mx-0 self-center">
-            <img src="images/profile.png" />
-          </div>
-          <div className="hidden sm:block">
-            <div className="text-white font-bold">Username</div>
-            <div className="text-xs">View Profile</div>
-          </div>
-          <div className="hidden sm:block self-center justify-self-end">
-            <Icon.KebabMenu />
+          <NavLink />
+          <div className="border-b my-10 border-b-special-bg"></div>
+          <div className="flex justify-between">
+            <div className="mx-auto sm:mx-0 self-center">
+              <img src="images/profile.png" />
+            </div>
+            <div className="hidden sm:block">
+              <div className="text-white font-bold">Username</div>
+              <div className="text-xs">View Profile</div>
+            </div>
+            <div className="hidden sm:block self-center justify-self-end">
+              <Icon.KebabMenu />
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
