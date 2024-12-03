262979fb611710d3e52abb0b32971a95b49bbaad
stnDinus <111202214433@mhs.dinus.ac.id>
2024-12-03 10:15:31 +0700

    feat: transaction icons

--- src/data/transaction.jsx
+++ src/data/transaction.jsx
@@ -1,3 +1,5 @@
+import { Icon } from "../components/Elements/Icon";
+
 const transactions = [
   {
     id: 1,
@@ -8,7 +10,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 160,
     type: "Expense",
-    // icon: <Icon.Shopping />,
+    icon: <Icon.Shopping />,
   },
   {
     id: 2,
@@ -19,7 +21,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 20,
     type: "Expense",
-    // icon: <Icon.Shopping />,
+    icon: <Icon.Shopping />,
   },
   {
     id: 3,
@@ -30,7 +32,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 12,
     type: "Expense",
-    // icon: <Icon.Food />,
+    icon: <Icon.Food />,
   },
   {
     id: 4,
@@ -41,7 +43,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 15,
     type: "Expense",
-    // icon: <Icon.Food />,
+    icon: <Icon.Food />,
   },
   {
     id: 5,
@@ -52,7 +54,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 10,
     type: "Expense",
-    // icon: <Icon.Transportation />,
+    icon: <Icon.Transportation />,
   },
   {
     id: 6,
@@ -63,7 +65,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 20,
     type: "Expense",
-    // icon: <Icon.Food />,
+    icon: <Icon.Food />,
   },
   {
     id: 7,
@@ -74,7 +76,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 30,
     type: "Expense",
-    // icon: <Icon.Shopping />,
+    icon: <Icon.Shopping />,
   },
   {
     id: 8,
@@ -85,7 +87,7 @@ const transactions = [
     paymentMethod: "Credit Card",
     amount: 100,
     type: "Revenue",
-    // icon: <Icon.Others />,
+    icon: <Icon.Others />,
   },
 ];
 
