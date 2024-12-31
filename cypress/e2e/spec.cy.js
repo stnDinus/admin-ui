a5a9cdc22896885713d00bad7c58ef9982d8bcde
arsmoriendy <nugrahasteven@gmail.com>
2024-12-31 09:53:36 +0700

    feat: cypress testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,73 @@
+describe('Access', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173')
+  })
+})
+
+describe("User login", () => {
+  it("should allow user to log in with valid credentials", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.url().should("include", "/login");
+
+    cy.get("input#email")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "hello@example.com")
+      .type("hello@example.com")
+      .should("have.value", "hello@example.com");
+
+    cy.get("input#password")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "*************")
+      .type("123456")
+      .should("have.value", "123456");
+
+    cy.get("button").contains("Login").click();
+
+    cy.get("nav");
+
+    cy.get("header");
+  });
+
+  it("should not allow user to log in with invalid credentials", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.url().should("include", "/login");
+
+    cy.get("input#email")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "hello@example.com")
+      .type("hello@example.com")
+      .should("have.value", "hello@example.com");
+
+    cy.get("input#password")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "*************")
+      .type("123")
+      .should("have.value", "123");
+
+    cy.get("button").contains("Login").click();
+
+    cy.get("div").contains("Wrong Password");
+  });
+
+  it("should prohibit user to log in with invalid email format", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.url().should("include", "/login");
+
+    cy.get("input#email")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "hello@example.com")
+      .type("helloexample.com")
+      .should("have.value", "helloexample.com");
+
+    cy.get("input#password")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "*************")
+      .type("123")
+      .should("have.value", "123");
+
+    cy.get("div").contains("Invalid email address format");
+  });
+});
