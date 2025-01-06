c1a8416e0ce44afe0e70963d5d0d06f584a2fb7f
stnDinus <111202214433@mhs.dinus.ac.id>
2025-01-06 20:33:29 +0700

    feat: goals card loading spinner

--- src/components/Fragments/Dashboard/CardGoal.jsx
+++ src/components/Fragments/Dashboard/CardGoal.jsx
@@ -6,11 +6,14 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { NotifContext } from "../../../context/notifContext";
 import { AuthContext } from "../../../context/authContext";
+import { CircularProgress } from "@mui/material";
 
 const CardGoal = () => {
   const [goals, setGoals] = useState({ presentAmount: 0, targetAmount: 0 });
   const { setMsg, setOpen } = useContext(NotifContext);
   const { setIsLoggedIn, setName } = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const value = (goals.presentAmount * 100) / goals.targetAmount;
 
@@ -51,6 +54,8 @@ const CardGoal = () => {
           console.log(error.response);
         }
       }
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -62,57 +67,63 @@ const CardGoal = () => {
     <Card
       title="Goals"
       desc={
-        <div className="p-2">
-          <div className="flex justify-between">
-            <div className="flex">
-              <span className="text-2xl font-bold me-4 self-center">
-                ${goals.presentAmount}
-              </span>
-              <div className="p-2 bg-gray-05 rounded-md box-border">
-                <Icon.Edit />
-              </div>
-            </div>
-            <div>Nov, 2023</div>
+        isLoading ? (
+          <div className="grid place-items-center h-full">
+            <CircularProgress color="inherit" className="w-full h-full" />
           </div>
-          <div className="border-b-2 my-4"></div>
-          <div className="flex justify-between">
-            <div>
-              <div className="flex mt-3 mb-10">
-                <div>
-                  <Icon.Award />
-                </div>
-                <div className="ms-2">
-                  <span className="text-gray-02">Target Achieved</span>
-                  <br />
-                  <span className="font-bold text-xl">
-                    ${goals.targetAmount}
-                  </span>
-                </div>
-              </div>
+        ) : (
+          <div className="p-2">
+            <div className="flex justify-between">
               <div className="flex">
-                <div>
-                  <Icon.Target />
-                </div>
-                <div className="ms-2">
-                  <span className="text-gray-02">This Month Target</span>
-                  <br />
-                  <span className="font-bold text-xl">
-                    ${goals.presentAmount}
-                  </span>
+                <span className="text-2xl font-bold me-4 self-center">
+                  ${goals.presentAmount}
+                </span>
+                <div className="p-2 bg-gray-05 rounded-md box-border">
+                  <Icon.Edit />
                 </div>
               </div>
+              <div>Nov, 2023</div>
             </div>
-            <div className="ms-4 text-center">
-              <CompositionExample desc={value} />
-              <div className="flex justify-between">
-                <span className="text-gray-03">$0</span>
-                <span className="font-bold text-2xl">12K</span>
-                <span className="text-gray-03">$20K</span>
+            <div className="border-b-2 my-4"></div>
+            <div className="flex justify-between">
+              <div>
+                <div className="flex mt-3 mb-10">
+                  <div>
+                    <Icon.Award />
+                  </div>
+                  <div className="ms-2">
+                    <span className="text-gray-02">Target Achieved</span>
+                    <br />
+                    <span className="font-bold text-xl">
+                      ${goals.targetAmount}
+                    </span>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div>
+                    <Icon.Target />
+                  </div>
+                  <div className="ms-2">
+                    <span className="text-gray-02">This Month Target</span>
+                    <br />
+                    <span className="font-bold text-xl">
+                      ${goals.presentAmount}
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="mt-2">Target vs Achievement</div>
+              <div className="ms-4 text-center">
+                <CompositionExample desc={value} />
+                <div className="flex justify-between">
+                  <span className="text-gray-03">$0</span>
+                  <span className="font-bold text-2xl">12K</span>
+                  <span className="text-gray-03">$20K</span>
+                </div>
+                <div className="mt-2">Target vs Achievement</div>
+              </div>
             </div>
           </div>
-        </div>
+        )
       }
     />
   );
