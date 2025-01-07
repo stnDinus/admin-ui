6c08099a7fbf252adc042ff32a6dc3174799d618
stnDinus <111202214433@mhs.dinus.ac.id>
2025-01-07 14:26:10 +0700

    fix: persist name

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,13 +1,16 @@
+import { jwtDecode } from "jwt-decode";
 import { createContext, useState } from "react";
 
-const INITIAL_STATE = localStorage.getItem("refreshToken") ? true : false;
+const refreshToken = localStorage.getItem("refreshToken");
+const INITIAL_LOGGED_IN = refreshToken ? true : false;
+const INITIAL_NAME = refreshToken ? jwtDecode(refreshToken)["name"] : "";
 
 /** @ts-ignore */
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(INITIAL_STATE);
-  const [name, setName] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(INITIAL_LOGGED_IN);
+  const [name, setName] = useState(INITIAL_NAME);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, name, setName }}>
